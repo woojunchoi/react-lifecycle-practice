595556eb109280cd80f5a1e007e2fc1fffe1ccb3
woojunchoi <tim.woojune1@gmail.com>
2018-03-08 22:29:03 -0800

    pokemon api added

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'whatwg-fetch';
+import axios from 'axios'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pokemon: []
+    };
+
+  }
+  
+  loadPokemon = (url) => {
+    axios.get(url)
+      .then(response => {
+        return response.data
+      }).then(json => {
+        let pages = Math.round(json.count / this.state.limit);
+        this.setState({
+          pokemon: json.results,
+          totalPages: pages,
+          count: json.count
+        });
+
+      }).catch(err => {
+        console.log(err)
+      })
+  }
+
+  componentDidMount() {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          <h2></h2>
       </div>
     );
   }
