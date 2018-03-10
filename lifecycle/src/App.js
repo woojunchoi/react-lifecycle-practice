2091f77a32e5c3c497633cd2e2bd36e477b0b99f
woojunchoi <tim.woojune1@gmail.com>
2018-03-10 00:01:11 -0800

    componentwillunmount added

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -32,17 +32,32 @@ class App extends Component {
   }
 
   componentWillMount() {
-    console.log('1')
+    console.log('parent componentwillmount')
+    // this.setState({count:500})
   }
   componentDidMount() {
-    console.log('2')
+    console.log('parent componentdidmount')
     this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
   }
 
+  changeState = () => {
+    this.setState({count:10})
+  }
+
+  unmount = () => {
+    this.setState({count:50})
+  }
   render() {
+    if(this.state.count === 50) {
+      return(<div></div>)
+    }
+    console.log('render')
     return (
       <div>
+          parent:{this.state.count}
           <Test count = {this.state.count} />
+          <button onClick={() => this.changeState()}>button</button>
+          <button onClick={this.unmount}>unmount</button>
       </div>
     );
   }
--- lifecycle/src/test.js
+++ lifecycle/src/test.js
@@ -4,18 +4,43 @@ class Test extends Component {
     constructor(props) {
         super(props)
     }
+    
+    componentWillMount() {
+        console.log('child componentwillmount')
+    }
+
+    componentDidMount() {
+        console.log('child comonentdidmount')
+    }
+
+    //can compare previous props and updated props here setstate possible
     componentWillReceiveProps(nextProps) {
-        console.log(this.props)
+        console.log('child componentwillreceiveprops')
+        // console.log(nextProps)
+    }
+    // even though props has been changed,
+    //  this component does not render because of shouldcomponentupdate
+    // setstate is not possible
+    // default return true(which allows rerender)
+    shouldComponentUpdate(nextProps,nextState) {
+        return this.props !== nextProps
+    }
+    changeState = () => {
+        this.setState({count:10})
+    }
+    //you cant do setstate here. it will lead you to inifinite loop
+    componentWillUpdate(nextProps,nextState) {
         console.log(nextProps)
     }
-    // even though props has been changed, this component does not render because of shouldcomponentupdate
-    shouldComponentUpdate(nextProps,nextState) {
-        return this.props === nextProps
+    componentWillUnmount() {
+        console.log('child componentwillunmount')
     }
 
     render() {
+        console.log('child render')
         return(
-            <div>{this.props.count}</div>
+            <div>{this.props.count}
+            </div>
         )
     }
 
